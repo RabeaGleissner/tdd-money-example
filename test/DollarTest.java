00e26340c57a44d8b4f7fd7ff974b3efe554b61e
Rabea Gleissner <rabeagleissner@gmail.com>
2015-11-19 17:33:16 +0000

    add junit

--- test/DollarTest.java
+++ test/DollarTest.java
@@ -1,6 +1,9 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class DollarTest {
+    @Test
     public void testMultiplication() {
         Dollar five= new Dollar(5);
         five.times(2);
