1f7bb02ff0b4363c69b8c5f7f48ab62c18498613
Rabea Gleissner <rabeagleissner@gmail.com>
2015-11-19 17:47:05 +0000

    use variable instead of hard coded 5

--- src/Dollar.java
+++ src/Dollar.java
@@ -1,9 +1,9 @@
 public class Dollar {
     int amount;
     void times(int multiplier) {
-        amount = 5*2;
+        amount = amount*2;
     }
     Dollar(int amount) {
-
+        this.amount = amount;
     }
 }
