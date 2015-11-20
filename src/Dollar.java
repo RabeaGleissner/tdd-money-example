2aaf231e24ac8fe556970e3380ec25249ab271ea
Rabea Gleissner <rabeagleissner@gmail.com>
2015-11-20 10:11:11 +0000

    adjust variable name to be in line with class name

--- src/Dollar.java
+++ src/Dollar.java
@@ -6,7 +6,7 @@ public class Dollar extends Money {
         this.amount = amount;
     }
     public boolean equals (Object object) {
-        Money dollar= (Money) object;
-        return amount == dollar.amount;
+        Money money= (Money) object;
+        return amount == money.amount;
     }
 }
