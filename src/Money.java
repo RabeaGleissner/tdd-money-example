27a279d5e170fcffe63405645eddf04fa908c959
Rabea Gleissner <rabeagleissner@gmail.com>
2015-11-22 12:08:55 +0000

    plus method returns Sum

--- src/Money.java
+++ src/Money.java
@@ -33,6 +33,6 @@ class Money implements Expression {
     }
 
     Expression plus(Money addend) {
-        return new Money(amount + addend.amount, currency);
+        return new Sum(this, addend);
     }
 }
