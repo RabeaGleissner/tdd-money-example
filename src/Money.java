841c2a77c174e057a5adfd4198ed54f3a248a810
Rabea Gleissner <rabeagleissner@gmail.com>
2015-11-22 17:04:41 +0000

    declare times in Expressions and make times method pulic

--- src/Money.java
+++ src/Money.java
@@ -1,5 +1,5 @@
 class Money implements Expression {
-    Expression times(int multiplier) {
+    public Expression times(int multiplier) {
         return new Money(amount * multiplier, currency);
     }
 
