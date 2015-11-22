8b0a5ec6b686b83af123dcdf8ad7b18aab9bf503
Rabea Gleissner <rabeagleissner@gmail.com>
2015-11-22 11:15:41 +0000

    remove references to Franc and Dollar, delete subclasses

--- src/Dollar.java
+++
@@ -1,9 +0,0 @@
-public class Dollar extends Money {
-
-    static Money dollar(int amount) {
-        return new Money(amount, "USD");
-    }
-    Dollar(int amount, String currency) {
-        super(amount, currency);
-    }
-}
--- src/Franc.java
+++
@@ -1,9 +0,0 @@
-public class Franc extends Money {
-
-    static Money franc(int amount) {
-        return new Money(amount, "CHF");
-    }
-    Franc (int amount, String currency) {
-        super(amount, currency);
-    }
-}
