6b72b741126481dbd6776dc5128f5b7d50fabab5
Rabea Gleissner <rabeagleissner@gmail.com>
2015-11-22 14:59:38 +0000

    look up rate

--- src/Bank.java
+++ src/Bank.java
@@ -1,19 +1,18 @@
+import java.util.Hashtable;
+
 public class Bank {
     Money reduce(Expression source, String to) {
         return source.reduce(this, to);
     }
 
-    public void addRate(String chf, String usd, int i) {
-    }
     int rate(String from, String to) {
-        return (from.equals("CHF") && to.equals("USD"))
-                ? 2
-                : 1;
+        Integer rate= (Integer) rates.get(new Pair(from, to));
+        return rate.intValue();
     }
 
     private Hashtable rates= new Hashtable();
 
     void addRate(String from, String to, int rate) {
-        rates.put(new Pair(from, to)), new Integer(rate));
+        rates.put(new Pair(from, to), new Integer(rate));
     }
 }
