aed23189ba6f49fbb9f42ad221e417eb911b5349
Rabea Gleissner <rabeagleissner@gmail.com>
2015-11-22 13:09:55 +0000

    add reduce method to Expressions interface

--- src/Money.java
+++ src/Money.java
@@ -35,4 +35,8 @@ class Money implements Expression {
     Expression plus(Money addend) {
         return new Sum(this, addend);
     }
+
+    public Money reduce(String to) {
+        return this;
+    }
 }
