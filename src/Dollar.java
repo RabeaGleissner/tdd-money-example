a574f94c8bca566e5edf6a4814f1647b18659636
Rabea Gleissner <rabeagleissner@gmail.com>
2015-11-22 10:56:21 +0000

    replace references to the subclasses with references to Money class

--- src/Dollar.java
+++ src/Dollar.java
@@ -1,5 +1,8 @@
 public class Dollar extends Money {
 
+    static Money dollar(int amount) {
+        return new Money(amount, "USD");
+    }
     Dollar(int amount, String currency) {
         super(amount, currency);
     }
--- src/Franc.java
+++ src/Franc.java
@@ -1,5 +1,8 @@
 public class Franc extends Money {
 
+    static Money franc(int amount) {
+        return new Money(amount, "CHF");
+    }
     Franc (int amount, String currency) {
         super(amount, currency);
     }
