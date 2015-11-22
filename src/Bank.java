9d6eb6b7843f5eb260848306c11bc58982ba5b64
Rabea Gleissner <rabeagleissner@gmail.com>
2015-11-22 13:33:13 +0000

    pass Bank to reduce mehod

--- src/Bank.java
+++ src/Bank.java
@@ -1,6 +1,6 @@
 public class Bank {
     Money reduce(Expression source, String to) {
-        return source.reduce(to);
+        return source.reduce(this, to);
     }
 
     public void addRate(String chf, String usd, int i) {
