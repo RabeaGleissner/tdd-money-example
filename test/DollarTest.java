f732f7a6828e3495d2b14afcdfb909949c65c8c7
Rabea Gleissner <rabeagleissner@gmail.com>
2015-11-22 15:00:13 +0000

    test for exchange rate from USD to USD should return 1

--- test/DollarTest.java
+++ test/DollarTest.java
@@ -71,4 +71,10 @@ public class DollarTest {
     public void testArrayEquals() {
         assertEquals(new Object[] {"abc"}, new Object[] {"abc"});
     }
+
+    @Test
+    public void testIdentityRate() {
+        assertEquals(1, new Bank().rate("USD", "USD"));
+
+    }
 }
